958947830c517c703d867d0436aa83e60d3c2e77
EhapAshraf <118015474+EhapAshraf@users.noreply.github.com>
2022-12-01 14:13:38 +0200

    Delete c++.py

--- c++.py
+++
@@ -1,9 +0,0 @@
-f2=open("file5.cpp","w+")
-f2.write("#include<iostream>\nint main(){\nstd::cout<<\"")
-message=input("please enter the message : ")
-f2.write(message)
-f2.write("\"<<std::endl;\n}")
-
-f2.close()
-subprocess.call(["g++", "file5.cpp"]) 
-subprocess.call("a",shell=True)
